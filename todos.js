85572fd1ab4171a220ab03a328522138404f118c
jcyoder <kajcyoder@comcast.net>
2014-05-08 08:17:08 -0600

    Added a call to setListeners and added console.logs to verify the events were being called.

--- todos.js
+++ todos.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
     
     function setListeners() {
         
-        $('#new-todo').keyup(function(event) {
+        $('#new-todo').keyup(function (event) {
             //13 is the enter key
-            if(event.which === 13) {
+            if (event.which === 13) {
                 console.log("enter key was pressed");
                 // get the entry
                 var listentry = this.value;
@@ -16,31 +16,35 @@ $(document).ready(function() {
                 //display the 'X'
                 //if not already shown, display the footer info  
             } 
-            console.log("leaving keyup");
             
         });
         
-        $('#todo-list li').dblclick(function() {
+        $('#todo-list li').dblclick(function () {
             //if double click on the list item then can edit it
-            
+            console.log("Double click event"); 
         });
         
-        $('#todo-list li').keyup(function(event) {
+        $('#todo-list li').keyup(function (event) {
             //if press the escape key then throw away the edits and keep what you had
-            if(event.which === 27) {
-                
+            if (event.which === 27) {
+                console.log("Escape was clicked");
             }
         });
-      /*  
-        $('.toggle').click()  {
+       
+        $('.toggle').click(function ()  {
+            console.log("Clicked on a checkmark");
             //click on a checkmark
-        }
+            
+        });
         
-        $('#toggle-all').click()  {
+        $('#toggle-all').click( function ()  {
             //click on the chevron
-        }
-       */ 
+            console.log("clicked on the chevron");
+        });
+        
+        
+        
     }
     
-    
+    setListeners();
 });
