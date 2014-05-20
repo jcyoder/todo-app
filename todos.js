5f0f7f682c9a1777557bc89dc144cb30b301bf32
jcyoder <kajcyoder@comcast.net>
2014-05-20 15:43:40 -0600

    removed unused code

--- todos.js
+++ todos.js
@@ -209,21 +209,7 @@ $(document).ready(function () {
                 countstring = numactive + " items left";
                 $('#footer #todo-count').text(countstring);
             }
-       /*   
-            $('#footer #todo-count #numcountstyle').text(numactive);
-            if (numactive === 1) {
-                $('#footer #todo-count').text(" item left");
-                
-                // countstring = numactive + " item left";
-             //  $('#footer #todo-count').text(" item left");     
-            } else {
-                var  finalstr = $('#footer #todo-count').text().replace("item", "items");
-           //     $('#footer #todo-count').text("items left");
-          //      $('#footer #todo-count #numcountstyle').text(numactive);
-               // countstring = numactive + " items left";
-               
-            }
-            */
+            
         }
     };
     
